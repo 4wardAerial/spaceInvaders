d1d029032dc5c22b809303a1d16803382e9a3ce5
4wardAerial <4lucas.h.bertanha8@gmail.com>
2025-01-16 21:22:32 -0300

    Makes movement slightly better

--- objects/player/playerBody.gd
+++ objects/player/playerBody.gd
@@ -1,11 +1,41 @@
 extends CharacterBody2D
 
-const SPEED : float = 300.0
+const SPEED : float = 100.0
+const MIN_VELOCITY : float = 10.0
+const MAX_VELOCITY : float = 100.0
+const ACCEL : float = 2.0
+const DRAG : float = 1.0                                                                          
 
-func get_input():
+func movement_input(delta) -> void:
+	# Makes the player look at the mouse and move to it in a straight line 
 	look_at(get_global_mouse_position())
-	velocity = transform.x * Input.get_action_strength("move") * SPEED
 	
+	if Input.is_action_pressed("move"):
+		print("PRESSING")
+		print(velocity)
+		
+		velocity += transform.x * Input.get_action_strength("move") * SPEED * ACCEL * delta
+		
+	else:
+		print("NOT PRESSING")
+		print(velocity)
+		
+		# Reduces velocity until full stop
+		if velocity.length() > MIN_VELOCITY:
+			print("DEACCELERATING")
+			velocity -= velocity * DRAG * delta
+		
+		# Stops when size of the vector is smaller the min
+		else:
+			velocity = Vector2.ZERO
+
+func specials_input() -> void:
+	if Input.is_action_just_pressed("special1"):
+		print("SPECIAL 1")
+
+func _process(delta: float) -> void:
+	specials_input()
+
 func _physics_process(delta: float) -> void:
-	get_input()
+	movement_input(delta)
 	move_and_slide()
